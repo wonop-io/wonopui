e09d74234bbd25a4b0fbdeec6fffaee098f7f6c9
Test User <test@example.com>
2025-06-07 19:20:29 +0200

    Exporting hook

--- src/components/overlays/mod.rs
+++ src/components/overlays/mod.rs
@@ -1,39 +1,52 @@
 // Overlay components
 
+#[cfg(feature = "Accordion")]
+mod accordion;
+#[cfg(feature = "Collapsible")]
+mod collapsible;
+#[cfg(feature = "Command")]
+mod command;
+#[cfg(feature = "ContextMenu")]
+mod context_menu;
 #[cfg(feature = "Dialog")]
 mod dialog;
 #[cfg(feature = "Drawer")]
 mod drawer;
-#[cfg(feature = "Popover")]
-mod popover;
 #[cfg(feature = "Dropdown")]
 mod dropdown;
-#[cfg(feature = "ContextMenu")]
-mod context_menu;
-#[cfg(feature = "Command")]
-mod command;
-#[cfg(feature = "Collapsible")]
-mod collapsible;
 #[cfg(feature = "GroupButton")]
 mod group_button;
-#[cfg(feature = "Accordion")]
-mod accordion;
-
-#[cfg(feature = "Dialog")]
-pub use dialog::{Dialog, DialogBody, DialogClose, DialogFooter, DialogHeader, DialogProvider, DialogTitle, DialogTrigger};
-#[cfg(feature = "Drawer")]
-pub use drawer::{Drawer, DrawerClose, DrawerDescription, DrawerFooter, DrawerHeader, DrawerProvider, DrawerSide, DrawerTitle, DrawerTrigger};
 #[cfg(feature = "Popover")]
-pub use popover::{Popover, PopoverContent, PopoverPosition, PopoverTrigger};
-#[cfg(feature = "Dropdown")]
-pub use dropdown::{Dropdown, DropdownItem, DropdownProps};
-#[cfg(feature = "ContextMenu")]
-pub use context_menu::{ContextMenu, ContextMenuCheckboxItem, ContextMenuContent, ContextMenuItem, ContextMenuLabel, ContextMenuRadioGroup, ContextMenuRadioItem, ContextMenuSeparator, ContextMenuShortcut, ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger, ContextMenuTrigger};
-#[cfg(feature = "Command")]
-pub use command::Command;
-#[cfg(feature = "Collapsible")]
-pub use collapsible::{Collapsible, CollapsibleContent, CollapsibleHeader, CollapsibleItem, CollapsibleTitle, CollapsibleTrigger};
-#[cfg(feature = "GroupButton")]
-pub use group_button::{GroupButton, GroupButtonTrigger};
+mod popover;
+
 #[cfg(feature = "Accordion")]
 pub use accordion::Accordion;
+#[cfg(feature = "Collapsible")]
+pub use collapsible::{
+    Collapsible, CollapsibleContent, CollapsibleHeader, CollapsibleItem, CollapsibleTitle,
+    CollapsibleTrigger,
+};
+#[cfg(feature = "Command")]
+pub use command::Command;
+#[cfg(feature = "ContextMenu")]
+pub use context_menu::{
+    ContextMenu, ContextMenuCheckboxItem, ContextMenuContent, ContextMenuItem, ContextMenuLabel,
+    ContextMenuRadioGroup, ContextMenuRadioItem, ContextMenuSeparator, ContextMenuShortcut,
+    ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger, ContextMenuTrigger,
+};
+#[cfg(feature = "Dialog")]
+pub use dialog::{
+    use_dialog, Dialog, DialogBody, DialogClose, DialogFooter, DialogHeader, DialogProvider,
+    DialogTitle, DialogTrigger,
+};
+#[cfg(feature = "Drawer")]
+pub use drawer::{
+    Drawer, DrawerClose, DrawerDescription, DrawerFooter, DrawerHeader, DrawerProvider, DrawerSide,
+    DrawerTitle, DrawerTrigger,
+};
+#[cfg(feature = "Dropdown")]
+pub use dropdown::{Dropdown, DropdownItem, DropdownProps};
+#[cfg(feature = "GroupButton")]
+pub use group_button::{GroupButton, GroupButtonTrigger};
+#[cfg(feature = "Popover")]
+pub use popover::{Popover, PopoverContent, PopoverPosition, PopoverTrigger};
